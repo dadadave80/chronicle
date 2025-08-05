f3182ecb217e4b727a0f57c728fecf11d3beca48
David Dada <daveproxy80@gmail.com>
2025-08-05 23:09:58 +0100

    feat: add external party facet

---
+++ src/facets/PartiesFacet.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Role} from "@chainsight-types/PartyTypes.sol";
+import {LibParty} from "@chainsight/libraries/LibParty.sol";
+
+contract PartiesFacet {
+    using LibParty for *;
+
+    function registerParty(string calldata _name, Role _role) external {
+        _name._registerParty(_role);
+    }
+
+    function isActiveRole(address _addr, Role _role) public view returns (bool) {
+        return _addr._isActiveRole(_role);
+    }
+
+    function getAllActiveParties() public view returns (address[] memory) {
+        return LibParty._getActiveParties();
+    }
+
+    function getPartiesByRole(Role _role) public view returns (address[] memory) {
+        return _role._getPartiesByRole();
+    }
+
+    function getManufacturers() public view returns (address[] memory) {
+        return LibParty._getManufacturers();
+    }
+
+    function getSuppliers() public view returns (address[] memory) {
+        return LibParty._getSuppliers();
+    }
+
+    function getTransporters() public view returns (address[] memory) {
+        return LibParty._getTransporters();
+    }
+
+    function getRetailers() public view returns (address[] memory) {
+        return LibParty._getRetailers();
+    }
+}
