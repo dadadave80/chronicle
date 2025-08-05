221039948588244fe9d2c8c0b5abec80afa860c6
David Dada <daveproxy80@gmail.com>
2025-08-05 23:09:07 +0100

    feat: add party logs

---
+++ src/libraries/logs/PartyLogs.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {Role} from "@chainsight-types/PartyTypes.sol";
+
+event PartyRegistered(address indexed addr, string name, Role role);
+
+event PartyActivated(address indexed addr);
+
+event PartyDeactivated(address indexed addr);
