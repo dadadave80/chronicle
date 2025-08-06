fa8522d1b23c62b911f198cfcdf276f28d9089be
signor1 <emmanuelomemgboji@gmail.com>
2025-08-07 00:47:19 +0100

    feat: how it works

--- frontend/app/(guest)/page.tsx
+++ frontend/app/(guest)/page.tsx
@@ -1,5 +1,6 @@
 import Features from "@/components/guest/features";
 import HeroSection from "@/components/guest/hero-section";
+import HowItWorks from "@/components/guest/how-it-works";
 import WhyChronify from "@/components/guest/why-chronify";
 
 export default function Home() {
@@ -11,6 +12,8 @@ export default function Home() {
       <WhyChronify />
       {/* Features */}
       <Features />
+      {/* How It Works */}
+      <HowItWorks />
     </main>
   );
 }
