1a71ef1027221e40cd35affb8af025b3b0bf4d74
David Dada <daveproxy80@gmail.com>
2025-08-06 02:24:49 +0100

    feat: create functions to generate product(hedera) tokens and keys

--- src/libraries/LibProduct.sol
+++ src/libraries/LibProduct.sol
@@ -60,6 +60,28 @@ library LibProduct {
         emit ProductCreated(tokenAddress, sender);
     }
 
+    function _getProductToken(string calldata _name)
+        internal
+        view
+        returns (IHederaTokenService.HederaToken memory token_)
+    {
+        token_.name = _name;
+        token_.symbol = "CSP";
+        token_.treasury = address(this);
+        token_.tokenKeys = _getProductTokenKeys();
+    }
+
+    function _getProductTokenKeys() internal view returns (IHederaTokenService.TokenKey[] memory tokenKeys_) {
+        tokenKeys_ = new IHederaTokenService.TokenKey[](7);
+        tokenKeys_[0] = LibKeyHelper.getSingleKey(KeyType.ADMIN, KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[1] = LibKeyHelper.getSingleKey(KeyType.KYC, KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[2] = LibKeyHelper.getSingleKey(KeyType.FREEZE, KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[3] = LibKeyHelper.getSingleKey(KeyType.WIPE, KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[4] = LibKeyHelper.getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[5] = LibKeyHelper.getSingleKey(KeyType.FEE, KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[6] = LibKeyHelper.getSingleKey(KeyType.PAUSE, KeyValueType.CONTRACT_ID, address(this));
+    }
+
     // function _transferProduct(uint256 _id, address _to, Status _newStatus) internal {
 
     //     emit ProductTransferred(_id, msg.sender, _to, _newStatus);
