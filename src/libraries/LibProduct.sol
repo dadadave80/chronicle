b71454cd3b9fcebf89110d62af063e00107c48d1
David Dada <daveproxy80@gmail.com>
2025-08-06 02:39:03 +0100

    chore: utilize libraries for better readability

--- src/libraries/LibProduct.sol
+++ src/libraries/LibProduct.sol
@@ -17,6 +17,9 @@ import "@chainsight-logs/ProductLogs.sol";
 
 library LibProduct {
     using EnumerableSet for EnumerableSet.AddressSet;
+    using LibParty for address;
+    using LibKeyHelper for KeyType;
+    using LibHederaTokenService for IHederaTokenService.HederaToken;
 
     function _productStorage() internal pure returns (ProductStorage storage pds_) {
         assembly {
@@ -26,10 +29,9 @@ library LibProduct {
 
     function _createProduct(string calldata _name, uint256 _price) internal {
         address sender = LibContext._msgSender();
-        if (!LibParty._isActiveRole(sender, Role.Supplier)) revert("Not a Supplier");
+        if (!sender._isActiveRole(Role.Supplier)) revert("Not a Supplier");
 
-        (int256 responseCode, address tokenAddress) =
-            LibHederaTokenService.createNonFungibleToken(_getProductToken(_name));
+        (int256 responseCode, address tokenAddress) = _getProductToken(_name).createNonFungibleToken();
 
         if (responseCode != HederaResponseCodes.SUCCESS) revert("Failed to create product");
         if (tokenAddress == address(0)) revert("Invalid token address");
@@ -63,13 +65,13 @@ library LibProduct {
 
     function _getProductTokenKeys() internal view returns (IHederaTokenService.TokenKey[] memory tokenKeys_) {
         tokenKeys_ = new IHederaTokenService.TokenKey[](7);
-        tokenKeys_[0] = LibKeyHelper.getSingleKey(KeyType.ADMIN, KeyValueType.CONTRACT_ID, address(this));
-        tokenKeys_[1] = LibKeyHelper.getSingleKey(KeyType.KYC, KeyValueType.CONTRACT_ID, address(this));
-        tokenKeys_[2] = LibKeyHelper.getSingleKey(KeyType.FREEZE, KeyValueType.CONTRACT_ID, address(this));
-        tokenKeys_[3] = LibKeyHelper.getSingleKey(KeyType.WIPE, KeyValueType.CONTRACT_ID, address(this));
-        tokenKeys_[4] = LibKeyHelper.getSingleKey(KeyType.SUPPLY, KeyValueType.CONTRACT_ID, address(this));
-        tokenKeys_[5] = LibKeyHelper.getSingleKey(KeyType.FEE, KeyValueType.CONTRACT_ID, address(this));
-        tokenKeys_[6] = LibKeyHelper.getSingleKey(KeyType.PAUSE, KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[0] = KeyType.ADMIN.getSingleKey(KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[1] = KeyType.KYC.getSingleKey(KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[2] = KeyType.FREEZE.getSingleKey(KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[3] = KeyType.WIPE.getSingleKey(KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[4] = KeyType.SUPPLY.getSingleKey(KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[5] = KeyType.FEE.getSingleKey(KeyValueType.CONTRACT_ID, address(this));
+        tokenKeys_[6] = KeyType.PAUSE.getSingleKey(KeyValueType.CONTRACT_ID, address(this));
     }
 
     // function _transferProduct(uint256 _id, address _to, Status _newStatus) internal {
