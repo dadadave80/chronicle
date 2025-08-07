f060d761f77ef78bc8f1b23dfc1752474b9f39bf
David Dada <daveproxy80@gmail.com>
2025-08-07 17:31:34 +0100

    chore: move init htc keytypes to initializer contract

---
+++ src/initializers/InitHTCKeyTypes.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {LibKeyHelper} from "@chronicle/libraries/hts/LibKeyHelper.sol";
+import {KeyType, KeyHelperStorage} from "@chronicle-types/KeyHelperStorage.sol";
+
+contract InitHTCKeyTypes {
+    function initHTCKeyTypes() external {
+        KeyHelperStorage storage $ = LibKeyHelper._getKeyHelperStorage();
+        $.keyTypes[KeyType.ADMIN] = 1;
+        $.keyTypes[KeyType.KYC] = 2;
+        $.keyTypes[KeyType.FREEZE] = 4;
+        $.keyTypes[KeyType.WIPE] = 8;
+        $.keyTypes[KeyType.SUPPLY] = 16;
+        $.keyTypes[KeyType.FEE] = 32;
+        $.keyTypes[KeyType.PAUSE] = 64;
+    }
+}
