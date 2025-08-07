0efa843c9fe64019993947d0c796ceca87dde21a
signor1 <emmanuelomemgboji@gmail.com>
2025-08-07 15:13:30 +0100

    feat: dashboard layout

---
+++ frontend/utils/sidebarLinks.tsx
@@ -0,0 +1,35 @@
+import { SideBarLinksTypes } from "@/types";
+import { GoHome, GoGear } from "react-icons/go";
+import {
+  MdOutlineInventory,
+  MdOutlineLocalShipping,
+  MdOutlineAnalytics,
+} from "react-icons/md";
+
+export const SideBarLinks: SideBarLinksTypes[] = [
+  {
+    href: "/dashboard",
+    title: "Home",
+    icon: <GoHome className="w-[20px] h-[20px]" />,
+  },
+  {
+    href: "/dashboard/products",
+    title: "Products",
+    icon: <MdOutlineInventory className="w-[20px] h-[20px]" />,
+  },
+  {
+    href: "/dashboard/shipments",
+    title: "Shipments",
+    icon: <MdOutlineLocalShipping className="w-[20px] h-[20px]" />,
+  },
+  {
+    href: "/dashboard/analytics",
+    title: "Analytics",
+    icon: <MdOutlineAnalytics className="w-[20px] h-[20px]" />,
+  },
+  {
+    href: "/dashboard/account",
+    title: "Account",
+    icon: <GoGear className="w-[20px] h-[20px]" />,
+  },
+];
